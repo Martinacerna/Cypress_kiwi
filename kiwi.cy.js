9628075b0b0a46b6f1b6a7231073a4ceec9bd989
Martina Cerna <martina.cerna01@seznam.cz>
2022-06-04 15:14:11 +0200

    Add About us tests

--- kiwi.cy.js
+++ kiwi.cy.js
@@ -1,9 +1,23 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
+
 describe('kiwi.cy.js', () => {
   it('should visit', () => {
     cy.visit('https://jobs.kiwi.com/')
+
+
     cy.contains('Allow all cookies').click()
 
-      // got   > e.init is not a function error after clicking on "About us"
+    //should check that correct page is loaded
+    cy.url().should('include', 'kiwi.com')
+
+      // got   > e.init is not a function error after clicking on "About us" - error  turned off by using first function
     cy.contains('About us').click()
+    cy.url().should('include', 'about-us')
+    cy.contains('Communication is key')
   })
 })
+
