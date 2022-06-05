245bb587405c53f447a4ee404f26ba38c7db191e
Martina Cerna <martina.cerna01@seznam.cz>
2022-06-05 21:23:47 +0200

    Add tests for main page, About us and Locations

--- kiwi.cy.js
+++ kiwi.cy.js
@@ -5,19 +5,50 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 })
 
 describe('kiwi.cy.js', () => {
-  it('should visit', () => {
+  it('jobs.kiwi.com', () => {
     cy.visit('https://jobs.kiwi.com/')
 
-
-    cy.contains('Allow all cookies').click()
+    cy.contains('Allow all cookies').click({force:true})
 
     //should check that correct page is loaded
     cy.url().should('include', 'kiwi.com')
 
       // got   > e.init is not a function error after clicking on "About us" - error  turned off by using first function
-    cy.contains('About us').click()
+    cy.contains('About us').click({force:true})
+  })
+
+  it('About us', () => {
+    //should check that correct page is loaded
     cy.url().should('include', 'about-us')
     cy.contains('Communication is key')
   })
-})
 
+  it('Locations', () => {
+    cy.contains('Locations').click({force:true})
+    cy.contains('Allow all cookies').click({force:true})
+
+    // check that correct page is loaded
+    cy.url().should('include','loc')
+
+    //check that there is not location named 'Rondon'
+    cy.should('not.contain','Rondon')
+    cy.contains('London').click({force:true})
+
+    //checking that there is button named 'Facilities'
+    assert.equal('Facilities', 'Facilities', 'Click on Facilities')
+    cy.contains('Facilities').click({force:true})
+
+    //opening specific position
+    cy.contains('Office On-Site Support').click()
+
+    //trying not to select 'Saved jobs' instead of 'Save'
+    assert.notEqual('Save', 'Saved')
+    cy.get('.b-info .kw-ga-job-save').click()
+//full class selector: button button--width button--secondary js-jobs-save kw-ga-job-save
+
+    //checking that saved position of 'Office On-Site Support' is in 'Saved jobs'
+    cy.contains('Saved jobs').click({force: true})
+    cy.contains('Office On-Site Support')
+
+  })
+})
