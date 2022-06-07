67f946472ec46a38a84a4d08564ccbba687f808b
Martina Cerna <martina.cerna01@seznam.cz>
2022-06-08 00:04:45 +0200

    Add more tests

--- kiwi.cy.js
+++ kiwi.cy.js
@@ -30,7 +30,7 @@ describe('kiwi.cy.js', () => {
     // check that correct page is loaded
     cy.url().should('include','loc')
 
-    //check that there is not location named 'Rondon'
+    //check that there is not a location named 'Rondon'
     cy.should('not.contain','Rondon')
     cy.contains('London').click({force:true})
 
@@ -54,7 +54,7 @@ describe('kiwi.cy.js', () => {
     cy.visit('https://jobs.kiwi.com/locations/')
     cy.contains('Read more').click()
     cy.contains('Silicon Valley')
- })
+  })
 
 
   it('Teams', () => {
@@ -76,6 +76,8 @@ describe('kiwi.cy.js', () => {
     cy.contains('Saved jobs').click({force: true})
     cy.contains('Realtime Analyst')
   })
+
+
    it('Jobs', () => {
      cy.visit('https://jobs.kiwi.com/jobs/')
      cy.get('#cookiebanner')
@@ -89,15 +91,88 @@ describe('kiwi.cy.js', () => {
      cy.get('.paging__next').click() // clicking to "next page arrow"
      cy.get('.b-job__bottom .b-job__definition').contains('Brno')   // checking that positions are only for Brno (atm checks only first position)
 
-     // testing filter when at first writing title of position and then selecting locality => looks ok
+     // testing filter when at first writing title of position and then selecting locality => looks ok, need to add check that its correct
      cy.visit('https://jobs.kiwi.com/jobs/')
-     cy.get('.input-field--large')
-         .click()
+     cy.get('.input-field--large').click()
         .type('Junior{enter}')
 
      cy.get('.inp-items--filter .inp-items__item')
-         .contains('Brno')
-         .click()
+         .contains('Brno').click()
      cy.wait(1000)
    })
+
+  it('Jobs - selecting location first', () => {
+    cy.visit('https://jobs.kiwi.com/jobs/')
+    cy.get('#cookiebanner')
+         .contains('Allow all cookies').click()
+
+    //selecting PRG as location
+    cy.get('.inp-items--filter .inp-items__item') //class="inp-items inp-items--filter" and class="inp-items__item"
+         .contains('Prague').click()
+     cy.wait(1000) // waiting for filter to finish loading PRG positions
+
+    // typing name of position to search bar
+    cy.get('.input-field--large').click() //class="input-field input-field--icon input-field--large"
+        .type('Talent Sourcer{enter}')
+
+    //check that Location is correct
+    cy.get('.c-jobs__item .b-job__bottom .b-job__definition')
+        expect({ Location: 'Prague' }).to.eql({ Location: 'Prague' }) // atm checking first position
+
+    // other unsuccessful options:
+   // cy.get('.c-jobs__item .b-job__bottom .b-job__definition').should('be.Prague')
+    // cy.get('.c-jobs__item .b-job__bottom .b-job__definition').each(()=> {.should('be.Prague')})
+  })
+
+  it('Teams - test drop down menu', () => {
+    cy.visit('https://jobs.kiwi.com/teams/')
+
+    cy.get('#cookiebanner')
+     .contains('Allow all cookies').click()
+
+  //opening Facilities team positions and clicking on "Scroll down"
+    cy.contains('Facilities').click()
+    cy.get('.scroll__inner').click()
+
+    // opening drop down menu
+    cy.get('.b-dropdown .icon-svg').click()
+        .get('.b-dropdown__body')
+  })
+
+  it('How we hire', () => {
+    cy.visit('https://jobs.kiwi.com/how-we-hire/')
+
+    //checking that URL is correct, accepting cookies
+    cy.url().should('include', 'hire')
+    cy.get('#cookiebanner')
+     .contains('Allow all cookies').click()
+
+  })
+  it('FAQ', () => {
+    cy.visit('https://jobs.kiwi.com/faq/')
+
+    //removing cookies banner if its there
+    // cy.get('#cookiebanner')
+    //  .contains('Allow all cookies').click()
+
+    //opening all 5 FAQ at once
+    cy.get('.kw-ga-faq .icon-svg--plus').click({ multiple: true })
+  })
+
+  it('Life at Kiwi.com', () => {
+    cy.visit('https://jobs.kiwi.com/blog/')
+
+    cy.get('#cookiebanner')
+      .contains('Allow all cookies').click()
+
+    cy.contains('Dogs').click()
+    // cy.get('#cookiebanner')   removing cookies banner if its there
+    //   .contains('Allow all cookies').click()
+
+    // checking 'Share' button if it's working
+    cy.contains('Content Operations Manager')
+    cy.get('.sticky-item .b-share .icon-svg--share').click()
+
+  })
+
 })
